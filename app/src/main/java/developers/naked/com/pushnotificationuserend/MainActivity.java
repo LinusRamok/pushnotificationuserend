a68acde8a5b342e4aac8201d71e77616b651fece
LinusRamok <linussunil.dsk@gmail.com>
2018-02-09 10:53:54 +0530

    Initial commit

---
+++ app/src/main/java/developers/naked/com/pushnotificationuserend/MainActivity.java
@@ -0,0 +1,13 @@
+package developers.naked.com.pushnotificationuserend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
