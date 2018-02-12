db49eb0f5d15622a4b9be804cda069cb55cbd92d
LinusRamok <linussunil.dsk@gmail.com>
2018-02-12 13:49:03 +0530

    send notification to specific user

--- app/src/main/java/developers/naked/com/pushnotificationuserend/MainActivity.java
+++ app/src/main/java/developers/naked/com/pushnotificationuserend/MainActivity.java
@@ -10,22 +10,24 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-    String uName,Uid,ToUname,ToUid;
+    String user_name,user_id,ToUname,ToUid;
     Button save,send;
     EditText username,userid,tousername,touserid;
-    DatabaseReference mNotificationDatabase;
+    DatabaseReference mNotificationDatabase,mUserDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNotificationDatabase=FirebaseDatabase.getInstance().getReference().child("notification");
+        mNotificationDatabase=FirebaseDatabase.getInstance().getReference().child("notifications");
+        mUserDatabase=FirebaseDatabase.getInstance().getReference().child("Users");
         username=(EditText)findViewById(R.id.editText);
         userid=(EditText)findViewById(R.id.editText2);
         tousername=(EditText)findViewById(R.id.editText3);
@@ -35,15 +37,18 @@ public class MainActivity extends AppCompatActivity {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                uName=username.getText().toString();
-                Uid=userid.getText().toString();
+                user_name=username.getText().toString();
+                user_id=userid.getText().toString();
+                String device_token= FirebaseInstanceId.getInstance().getToken();
+                mUserDatabase.child(user_id).child("Name").setValue(user_name);
+                mUserDatabase.child(user_id).child("device_token").setValue(device_token);
             }
         });
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 HashMap<String,String> notificationData=new HashMap<>();
-                notificationData.put("from",uName);
+                notificationData.put("from",user_name);
                 ToUname=tousername.getText().toString();
                 ToUid=touserid.getText().toString();
                 mNotificationDatabase.child(ToUname).push().setValue(notificationData).addOnSuccessListener(new OnSuccessListener<Void>() {
--- app/src/main/java/developers/naked/com/pushnotificationuserend/MyFirebaseMessagingService.java
+++ app/src/main/java/developers/naked/com/pushnotificationuserend/MyFirebaseMessagingService.java
@@ -1,5 +1,9 @@
 package developers.naked.com.pushnotificationuserend;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,32 +18,29 @@ import static android.content.ContentValues.TAG;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        String notification_title=remoteMessage.getNotification().getTitle();
+        String notification_message=remoteMessage.getNotification().getBody();
+        String click_action=remoteMessage.getNotification().getClickAction();
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(notification_title)
+                        .setContentText(notification_message);
+        Intent resultIntent = new Intent(click_action);
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-                //scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                //handleNow();
-            }
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        int mNotificationId = (int) System.currentTimeMillis();
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
 }
